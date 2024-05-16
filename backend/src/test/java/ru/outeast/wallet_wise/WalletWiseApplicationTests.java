20dc4e11528c3a7573b2136044e0f4733c968eab
RaKaLuS <sir.d-i-a2014@mail.ru>
2024-05-16 13:52:25 +0700

    init commit backend

---
+++ backend/src/test/java/ru/outeast/wallet_wise/WalletWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.outeast.wallet_wise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
